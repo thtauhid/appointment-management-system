6ae1302f00e8b27188d8f483740767b00b56fca2
Tasnimul H. Tauhid <thtauhid.71@gmail.com>
2023-03-08 02:58:34 +0530

    added view engine

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 require('dotenv').config()
+const port = process.env.PORT || 3000
+
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000
+const path = require('path')
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+// Set view engine
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+app.use(express.static(path.join(__dirname, 'public')))
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  return res.render('index')
 })
 
 app.listen(port, () => {
